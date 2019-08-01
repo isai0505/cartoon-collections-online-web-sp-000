7fbde529d85205fdf279896aa893f5b70ebfbd88
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-01 13:50:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect {|words| word.capitalize!}.join(" !")
+  planeteer_calls.collect {|word| word.capitalize!}.join(" !")
 end
 
 def long_planeteer_calls# code an argument here
